064596c9211dac471ae91eb40d1c4d651a3685ae
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-06-15 15:26:26 +0100

    adding hello servlet

---
+++ src/main/java/com/springapp/practice/servlet/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.springapp.practice.servlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="helloservlet",urlPatterns="/helloservlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("Running Hello Servlet doGet method");
+    }
+
+}
