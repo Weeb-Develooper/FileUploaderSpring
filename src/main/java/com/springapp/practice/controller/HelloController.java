260ceff2e0f4aa85e52d12fa9d1a163f61ef89cc
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-06-15 15:11:17 +0100

    first commit

---
+++ src/main/java/com/springapp/practice/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.springapp.practice.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    private Map<String, Object> result = new HashMap<>();
+
+    @RequestMapping("/hello")
+    public Map<String, Object> hello(){
+        result.put("name","Osahon Jonathan-Odia");
+        result.put("city","Lagos");
+        return result;
+    }
+
+}
